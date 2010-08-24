d8474716fcbde47a768f59d79f25ae6588a01d56
Nils Braden <ttrss@nilsbraden.de>
2010-08-24 17:38:34 +0000

    - Allow own SSL-certificates with own Keystore on SDCard - Created new OnSharedPreferenceChangeListener

--- ttrss-reader/src/org/ttrssreader/controllers/Controller.java
+++ ttrss-reader/src/org/ttrssreader/controllers/Controller.java
@@ -1,12 +1,12 @@
 /*
  * Tiny Tiny RSS Reader for Android
- * 
+ *
  * Copyright (C) 2009 J. Devauchelle and contributors.
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 3 as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -27,15 +27,19 @@ import android.util.Log;
 public class Controller {
 
 	public final static String JSON_END_URL = "api/";
-	
+
 	private boolean mIsControllerInitialized = false;
 	private ITTRSSConnector mTTRSSConnector;
-	private String url = "";
 
 	private static final String mutex = "";
 	private static Controller mInstance = null;
 	private SharedPreferences prefs = null;
 	
+	private String url = "";
+	private boolean mTrustAllSsl;
+	private boolean mUseKeystore;
+	private String mKeystorePassword;
+
 	private boolean mAutomaticMarkRead;
 	private boolean mOpenUrlEmptyArticle;
 	private boolean mUpdateUnreadOnStartup;
@@ -43,20 +47,17 @@ public class Controller {
 	private boolean mUseVolumeKeys;
 	private boolean mVibrateOnLastArticle;
 	private boolean mWorkOffline;
-	
+
 	private boolean mDisplayVirtuals;
 	private boolean mDisplayUnreadInVirtualFeeds;
 	private boolean mAlwaysFullRefresh;
 	private boolean mUseSwipe;
 	private boolean mDisplayOnlyUnread;
 	private int mArticleLimit;
-	
+
 	private int mDatabaseVersion;
 	private long mLastUpdateTime;
-	
-	private Controller() {
-	}
-	
+
 	public static Controller getInstance() {
 		synchronized (mutex) {
 			if (mInstance == null) {
@@ -65,12 +66,12 @@ public class Controller {
 			return mInstance;
 		}
 	}
-	
+
 	public synchronized void initializeController(Context context) {
-		
+
 		prefs = PreferenceManager.getDefaultSharedPreferences(context);
-		
-		// Login
+
+		// Connection
 		url = prefs.getString(Constants.CONNECTION_URL, "http://localhost/");
 		if (!url.endsWith(JSON_END_URL)) {
 			if (!url.endsWith("/")) {
@@ -80,9 +81,11 @@ public class Controller {
 		}
 		String userName = prefs.getString(Constants.CONNECTION_USERNAME, "");
 		String password = prefs.getString(Constants.CONNECTION_PASSWORD, "");
-		boolean trustAllSsl = prefs.getBoolean(Constants.CONNECTION_TRUST_ALL_SLL, false); 
-		mTTRSSConnector = new TTRSSJsonConnector(url, userName, password, trustAllSsl);
-		
+		mTrustAllSsl = prefs.getBoolean(Constants.CONNECTION_TRUST_ALL_SSL, false);
+		mUseKeystore = prefs.getBoolean(Constants.CONNECTION_USE_KEYSTORE, false);
+		mKeystorePassword = prefs.getString(Constants.CONNECTION_KEYSTORE_PASSWORD, "");
+		mTTRSSConnector = new TTRSSJsonConnector(url, userName, password);
+
 		// Usage
 		mAutomaticMarkRead = prefs.getBoolean(Constants.USAGE_AUTOMATIC_MARK_READ, true);
 		mOpenUrlEmptyArticle = prefs.getBoolean(Constants.USAGE_OPEN_URL_EMPTY_ARTICLE, false);
@@ -91,7 +94,7 @@ public class Controller {
 		mUseVolumeKeys = prefs.getBoolean(Constants.USAGE_USE_VOLUME_KEYS, true);
 		mVibrateOnLastArticle = prefs.getBoolean(Constants.USAGE_VIBRATE_ON_LAST_ARTICLE, true);
 		mWorkOffline = prefs.getBoolean(Constants.USAGE_WORK_OFFLINE, false);
-		
+
 		// Display
 		mDisplayVirtuals = prefs.getBoolean(Constants.DISPLAY_SHOW_VIRTUAL, true);
 		mDisplayUnreadInVirtualFeeds = prefs.getBoolean(Constants.DISPLAY_SHOW_VIRTUAL_UNREAD, false);
@@ -104,7 +107,7 @@ public class Controller {
 			setArticleLimit(100);
 			Log.e(Utils.TAG, "DISPLAY_ARTICLE_LIMIT was not an integer value, using default value: " + mArticleLimit);
 		}
-		
+
 		// Internal Data
 		try {
 			mDatabaseVersion = Integer.parseInt(prefs.getString(Constants.DATABASE_VERSION, "0"));
@@ -115,201 +118,215 @@ public class Controller {
 		try {
 			mLastUpdateTime = Long.parseLong(prefs.getString(Constants.LAST_UPDATE_TIME, "0"));
 		} catch (ClassCastException e) {
+			setLastUpdateTime(0);
 			Log.e(Utils.TAG, "LAST_UPDATE_TIME was not a valid time value");
 		}
-		
+
 	}
-	
+
 	public synchronized void checkAndInitializeController(final Context context) {
 		if (!mIsControllerInitialized) {
 			initializeController(context);
 			mIsControllerInitialized = true;
 		}
 	}
-	
+
 	// **** Getter / Setter **********
-	// ******* USAGE-Options ****************************
 	
+	// ******* USAGE-Options ****************************
+
 	public String getUrl() {
 		return url;
 	}
 
-	
 	public void setUrl(String url) {
 		this.url = url;
 	}
-	
+
 	public ITTRSSConnector getTTRSSConnector() {
 		return mTTRSSConnector;
 	}
 	
+	public boolean isTrustAllSsl() {
+		return mTrustAllSsl;
+	}
+	
+	public void setTrustAllSsl(boolean trustAllSsl) {
+		put(Constants.CONNECTION_TRUST_ALL_SSL, trustAllSsl);
+		this.mTrustAllSsl = trustAllSsl;
+	}
+	
+	public boolean isUseKeystore() {
+		return mUseKeystore;
+	}
+	
+	public void setUseKeystore(boolean useKeystore) {
+		put(Constants.CONNECTION_USE_KEYSTORE, useKeystore);
+		this.mUseKeystore = useKeystore;
+	}
+	
+	public String getKeystorePassword() {
+		return mKeystorePassword;
+	}
+	
+	public void setKeystorePassword(String keystorePassword) {
+		put(Constants.CONNECTION_KEYSTORE_PASSWORD, keystorePassword);
+		this.mKeystorePassword = keystorePassword;
+	}
+
 	public boolean isAutomaticMarkRead() {
 		return mAutomaticMarkRead;
 	}
-	
+
 	public void setAutomaticMarkRead(boolean automaticMarkRead) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_AUTOMATIC_MARK_READ, automaticMarkRead);
-		editor.commit();
+		put(Constants.USAGE_AUTOMATIC_MARK_READ, automaticMarkRead);
 		this.mAutomaticMarkRead = automaticMarkRead;
 	}
-	
+
 	public boolean isOpenUrlEmptyArticle() {
 		return mOpenUrlEmptyArticle;
 	}
-	
+
 	public void setOpenUrlEmptyArticle(boolean openUrlEmptyArticle) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_OPEN_URL_EMPTY_ARTICLE, openUrlEmptyArticle);
-		editor.commit();
+		put(Constants.USAGE_OPEN_URL_EMPTY_ARTICLE, openUrlEmptyArticle);
 		this.mOpenUrlEmptyArticle = openUrlEmptyArticle;
 	}
-	
+
 	public boolean isUpdateUnreadOnStartup() {
 		return mUpdateUnreadOnStartup;
 	}
-	
+
 	public void setUpdateUnreadOnStartup(boolean mUpdateUnreadOnStartup) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_UPDATE_UNREAD_ON_STARTUP, mUpdateUnreadOnStartup);
-		editor.commit();
+		put(Constants.USAGE_UPDATE_UNREAD_ON_STARTUP, mUpdateUnreadOnStartup);
 		this.mUpdateUnreadOnStartup = mUpdateUnreadOnStartup;
 	}
-	
+
 	public boolean isRefreshSubData() {
 		return mRefreshSubData;
 	}
-	
+
 	public void setRefreshSubData(boolean refreshSubData) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_REFRESH_SUB_DATA, refreshSubData);
-		editor.commit();
+		put(Constants.USAGE_REFRESH_SUB_DATA, refreshSubData);
 		this.mRefreshSubData = refreshSubData;
 	}
-	
+
 	public boolean isUseVolumeKeys() {
 		return mUseVolumeKeys;
 	}
-	
+
 	public void setUseVolumeKeys(boolean useVolumeKeys) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_USE_VOLUME_KEYS, useVolumeKeys);
-		editor.commit();
+		put(Constants.USAGE_USE_VOLUME_KEYS, useVolumeKeys);
 		this.mUseVolumeKeys = useVolumeKeys;
 	}
-	
+
 	public boolean isVibrateOnLastArticle() {
 		return mVibrateOnLastArticle;
 	}
-	
+
 	public void setVibrateOnLastArticle(boolean vibrateOnLastArticle) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_VIBRATE_ON_LAST_ARTICLE, vibrateOnLastArticle);
-		editor.commit();
+		put(Constants.USAGE_VIBRATE_ON_LAST_ARTICLE, vibrateOnLastArticle);
 		this.mVibrateOnLastArticle = vibrateOnLastArticle;
 	}
-	
+
 	public boolean isWorkOffline() {
 		return mWorkOffline;
 	}
-	
+
 	public void setWorkOffline(boolean workOffline) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.USAGE_WORK_OFFLINE, workOffline);
-		editor.commit();
+		put(Constants.USAGE_WORK_OFFLINE, workOffline);
 		this.mWorkOffline = workOffline;
 	}
-	
+
 	// ******* DISPLAY-Options ****************************
-	
+
 	public boolean isDisplayVirtuals() {
 		return mDisplayVirtuals;
 	}
-	
+
 	public void setDisplayVirtuals(boolean displayVirtuals) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.DISPLAY_SHOW_VIRTUAL, displayVirtuals);
-		editor.commit();
+		put(Constants.DISPLAY_SHOW_VIRTUAL, displayVirtuals);
 		this.mDisplayVirtuals = displayVirtuals;
 	}
-	
+
 	public boolean isDisplayUnreadInVirtualFeeds() {
 		return mDisplayUnreadInVirtualFeeds;
 	}
-	
+
 	public void setDisplayUnreadInVirtualFeeds(boolean displayUnreadInVirtualFeeds) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.DISPLAY_SHOW_VIRTUAL_UNREAD, displayUnreadInVirtualFeeds);
-		editor.commit();
+		put(Constants.DISPLAY_SHOW_VIRTUAL_UNREAD, displayUnreadInVirtualFeeds);
 		this.mDisplayUnreadInVirtualFeeds = displayUnreadInVirtualFeeds;
 	}
-	
+
 	public boolean isAlwaysFullRefresh() {
 		return mAlwaysFullRefresh;
 	}
-	
+
 	public void setAlwaysFullRefresh(boolean alwaysFullRefresh) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.DISPLAY_ALWAYS_FULL_REFRESH, alwaysFullRefresh);
-		editor.commit();
+		put(Constants.DISPLAY_ALWAYS_FULL_REFRESH, alwaysFullRefresh);
 		this.mAlwaysFullRefresh = alwaysFullRefresh;
 	}
-	
+
 	public boolean isUseSwipe() {
 		return mUseSwipe;
 	}
-	
+
 	public void setUseSwipe(boolean useSwipe) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.DISPLAY_USE_SWIPE, useSwipe);
-		editor.commit();
+		put(Constants.DISPLAY_USE_SWIPE, useSwipe);
 		this.mUseSwipe = useSwipe;
 	}
-	
+
 	public boolean isDisplayOnlyUnread() {
 		return mDisplayOnlyUnread;
 	}
-	
+
 	public void setDisplayOnlyUnread(boolean displayOnlyUnread) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putBoolean(Constants.DISPLAY_ONLY_UNREAD, displayOnlyUnread);
-		editor.commit();
+		put(Constants.DISPLAY_ONLY_UNREAD, displayOnlyUnread);
 		this.mDisplayOnlyUnread = displayOnlyUnread;
 	}
-	
+
 	public int getArticleLimit() {
 		return mArticleLimit;
 	}
-	
+
 	public void setArticleLimit(int articleLimit) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(Constants.DISPLAY_ARTICLE_LIMIT, articleLimit+"");
-		editor.commit();
+		put(Constants.DISPLAY_ARTICLE_LIMIT, articleLimit+"");
 		this.mArticleLimit = articleLimit;
 	}
-	
+
 	// ******* Internal Data ****************************
-	
+
 	public long getLastUpdateTime() {
 		return mLastUpdateTime;
 	}
 
 	public void setLastUpdateTime(long lastUpdateTime) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(Constants.LAST_UPDATE_TIME, lastUpdateTime+"");
-		editor.commit();
+		put(Constants.LAST_UPDATE_TIME, lastUpdateTime+"");
 		this.mLastUpdateTime = lastUpdateTime;
 	}
-	
+
 	public int getDatabaseVersion() {
 		return mDatabaseVersion;
 	}
 
 	public void setDatabaseVersion(int databaseVersion) {
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(Constants.DATABASE_VERSION, databaseVersion+"");
-		editor.commit();
+		put(Constants.DATABASE_VERSION, databaseVersion+"");
 		this.mDatabaseVersion = databaseVersion;
 	}
 	
+	
+	/*
+	 * Generic method to insert values into the preferences store
+	 */
+	public void put(String constant, Object o) {
+		SharedPreferences.Editor editor = prefs.edit();
+		if (o instanceof String) {
+			String string = (String) o;
+			editor.putString(constant, string);
+		} else if (o instanceof Boolean) {
+			Boolean bool = (Boolean) o;
+			editor.putBoolean(constant, bool);
+		}
+		editor.commit();
+	}
+
 }
