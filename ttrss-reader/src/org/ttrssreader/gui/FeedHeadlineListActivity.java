650c1dd1c467324f563abd2e59a5f6ac081c4bbf
nils braden <nils.braden@gmail.com>
2011-04-13 11:49:55 +0200

    Fixed swype-here-notification not beeing displayed in article-view

--- ttrss-reader/src/org/ttrssreader/gui/FeedHeadlineListActivity.java
+++ ttrss-reader/src/org/ttrssreader/gui/FeedHeadlineListActivity.java
@@ -339,16 +339,11 @@ public class FeedHeadlineListActivity extends MenuActivity {
         }
         
         // @formatter:off
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) { return false; }
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) { return false; }
-        @Override
-        public boolean onDown(MotionEvent e) { return false; }
-        @Override
-        public void onLongPress(MotionEvent e) { }
-        @Override
-        public void onShowPress(MotionEvent e) { }
+        @Override public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) { return false; }
+        @Override public boolean onSingleTapUp(MotionEvent e) { return false; }
+        @Override public boolean onDown(MotionEvent e) { return false; }
+        @Override public void onLongPress(MotionEvent e) { }
+        @Override public void onShowPress(MotionEvent e) { }
         // @formatter:on
     };
     
