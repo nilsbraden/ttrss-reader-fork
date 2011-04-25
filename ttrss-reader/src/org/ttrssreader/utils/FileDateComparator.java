536bdaf67d294dee693e96db278af39f6f14ae74
nils braden <nils.braden@gmail.com>
2011-04-25 19:39:07 +0200

    Added check for corrupted DB, it will be moved to a backup-file and a new DB will be created. Backup-files are managed, only two old backups will be stored in internal space, older backups are deleted.

---
+++ ttrss-reader/src/org/ttrssreader/utils/FileDateComparator.java
@@ -0,0 +1,43 @@
+/*
+ * ttrss-reader-fork for Android
+ * 
+ * Copyright (C) 2010 N. Braden.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 3 as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+package org.ttrssreader.utils;
+
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * Compares two files by their last-modified-date.
+ * 
+ * @author Nils Braden
+ *
+ */
+public class FileDateComparator implements Comparator<File> {
+    
+    @Override
+    public int compare(File f1, File f2) {
+        
+        long size1 = f1.lastModified();
+        long size2 = f2.lastModified();
+        
+        if (size1 < size2) {
+            return -1;
+        } else if (size1 > size2) {
+            return 1;
+        }
+        
+        return 0; // equal
+    }
+    
+}
