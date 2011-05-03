583b5f8063fd1e9166339fd089309d113a7a0281
Nils Braden <ttrss@nilsbraden.de>
2011-05-03 15:29:14 +0200

    - Refactored some DB-Operations. - Feeds are marked as read before new articles are inserted. Should fix the problem with old unread articles not beeing marked as read properly.

--- ttrss-reader/src/org/ttrssreader/gui/CategoryActivity.java
+++ ttrss-reader/src/org/ttrssreader/gui/CategoryActivity.java
@@ -42,7 +42,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
