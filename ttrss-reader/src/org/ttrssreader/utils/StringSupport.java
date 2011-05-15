61f71cb9d5bf5ffc06403120ba1b04fe2b1bc368
Nils Braden <ttrss@nilsbraden.de>
2011-05-15 13:37:54 +0200

    Removed unused code.

--- ttrss-reader/src/org/ttrssreader/utils/StringSupport.java
+++ ttrss-reader/src/org/ttrssreader/utils/StringSupport.java
@@ -1,11 +1,9 @@
 package org.ttrssreader.utils;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -165,35 +163,6 @@ public class StringSupport {
             return uri.getScheme() + "://" + uri.getAuthority();
         }
         return null;
-    }
-    
-    // TODO: See if this can be and/or needs to be optimized.
-    public static String convertStreamToString_OLD(InputStream is) {
-        /*
-         * To convert the InputStream to String we use the BufferedReader.readLine()
-         * method. We iterate until the BufferedReader return null which means
-         * there's no more data to read. Each line will be appended to a StringBuilder
-         * and returned as String.
-         */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is), 1024 * 10);
-        StringBuilder sb = new StringBuilder();
-        
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (OutOfMemoryError oome) {
-            oome.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (Exception e) {
-            }
-        }
-        return sb.toString();
     }
     
     public static String convertStreamToString(InputStream inputStream) throws IOException {
