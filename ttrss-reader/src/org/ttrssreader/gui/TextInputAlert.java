d9800f4c984ae25ba4f61ad7748f472c7e5d3b8d
Nils Braden <ttrss@nilsbraden.de>
2011-11-29 19:18:03 +0100

    Small changes to design of the box for the notes.

--- ttrss-reader/src/org/ttrssreader/gui/TextInputAlert.java
+++ ttrss-reader/src/org/ttrssreader/gui/TextInputAlert.java
@@ -1,10 +1,13 @@
 package org.ttrssreader.gui;
 
+import org.ttrssreader.R;
 import org.ttrssreader.gui.interfaces.TextInputAlertCallback;
 import org.ttrssreader.model.pojos.Article;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.EditText;
 
 public class TextInputAlert {
@@ -20,20 +23,22 @@ public class TextInputAlert {
     public void show(Context context) {
         AlertDialog.Builder alert = new AlertDialog.Builder(context);
         
-        alert.setTitle("Publish with a note");
-        alert.setMessage("");
+        alert.setTitle(context.getString(R.string.Commons_MarkPublishNote));
         
         final EditText input = new EditText(context);
+        input.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        input.setMinLines(3);
+        input.setMaxLines(10);
         alert.setView(input);
         
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        alert.setPositiveButton(context.getString(R.string.Utils_OkayAction), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 String value = input.getText().toString();
                 callback.onPublishNoteResult(article, value);
             }
         });
         
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        alert.setNegativeButton(context.getString(R.string.Utils_CancelAction), new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 dialog.dismiss();
             }
