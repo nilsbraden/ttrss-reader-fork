260abbb97e6c8058460734f75ded96606b6356a9
nils braden <nils.braden@gmail.com>
2012-03-27 23:15:19 +0200

    Added compatibility code. Resolved warnings.

--- ttrss-reader/src/org/ttrssreader/imageCache/ForegroundService.java
+++ ttrss-reader/src/org/ttrssreader/imageCache/ForegroundService.java
@@ -24,6 +24,7 @@ import org.ttrssreader.gui.interfaces.ICacheEndListener;
 import org.ttrssreader.utils.Utils;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.AsyncTask;
