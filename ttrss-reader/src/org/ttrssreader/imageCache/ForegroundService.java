7b53187730a40c7aa74210f87735714fba81a0ca
Nils Braden <ttrss@nilsbraden.de>
2013-03-17 16:11:10 +0100

    Removed old compatibility-code

--- ttrss-reader/src/org/ttrssreader/imageCache/ForegroundService.java
+++ ttrss-reader/src/org/ttrssreader/imageCache/ForegroundService.java
@@ -16,32 +16,17 @@
 
 package org.ttrssreader.imageCache;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import org.ttrssreader.R;
 import org.ttrssreader.controllers.Controller;
 import org.ttrssreader.gui.interfaces.ICacheEndListener;
 import org.ttrssreader.utils.AsyncTask;
 import org.ttrssreader.utils.Utils;
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.util.Log;
 
 public class ForegroundService extends Service implements ICacheEndListener {
-    private static final Class<?>[] mSetForegroundSignature = new Class[] { boolean.class };
-    private static final Class<?>[] mStartForegroundSignature = new Class[] { int.class, Notification.class };
-    private static final Class<?>[] mStopForegroundSignature = new Class[] { boolean.class };
-    
-    private NotificationManager mNM;
-    private Method mSetForeground;
-    private Method mStartForeground;
-    private Method mStopForeground;
-    private Object[] mSetForegroundArgs = new Object[1];
-    private Object[] mStartForegroundArgs = new Object[2];
-    private Object[] mStopForegroundArgs = new Object[1];
     
     public static final String ACTION_LOAD_IMAGES = "load_images";
     public static final String ACTION_LOAD_ARTICLES = "load_articles";
@@ -65,81 +50,9 @@ public class ForegroundService extends Service implements ICacheEndListener {
         parent = parentGUI;
     }
     
-    void invokeMethod(Method method, Object[] args) {
-        try {
-            method.invoke(this, mStartForegroundArgs);
-        } catch (InvocationTargetException e) {
-            // Should not happen.
-            Log.e(Utils.TAG, "Unable to invoke method", e);
-        } catch (IllegalAccessException e) {
-            // Should not happen.
-            Log.e(Utils.TAG, "Unable to invoke method", e);
-        }
-    }
-    
-    /**
-     * This is a wrapper around the new startForeground method, using the older
-     * APIs if it is not available.
-     */
-    void startForegroundCompat(int id, Notification notification) {
-        // If we have the new startForeground API, then use it.
-        if (mStartForeground != null) {
-            mStartForegroundArgs[0] = Integer.valueOf(id);
-            mStartForegroundArgs[1] = notification;
-            invokeMethod(mStartForeground, mStartForegroundArgs);
-            return;
-        }
-        
-        // Fall back on the old API.
-        mSetForegroundArgs[0] = Boolean.TRUE;
-        invokeMethod(mSetForeground, mSetForegroundArgs);
-        mNM.notify(id, notification);
-    }
-    
-    /**
-     * This is a wrapper around the new stopForeground method, using the older
-     * APIs if it is not available.
-     */
-    void stopForegroundCompat(int id) {
-        // If we have the new stopForeground API, then use it.
-        if (mStopForeground != null) {
-            mStopForegroundArgs[0] = Boolean.TRUE;
-            try {
-                mStopForeground.invoke(this, mStopForegroundArgs);
-            } catch (InvocationTargetException e) {
-                // Should not happen.
-                Log.e(Utils.TAG, "Unable to invoke stopForeground", e);
-            } catch (IllegalAccessException e) {
-                // Should not happen.
-                Log.e(Utils.TAG, "Unable to invoke stopForeground", e);
-            }
-            return;
-        }
-        
-        // Fall back on the old API. Note to cancel BEFORE changing the
-        // foreground state, since we could be killed at that point.
-        mNM.cancel(id);
-        mSetForegroundArgs[0] = Boolean.FALSE;
-        invokeMethod(mSetForeground, mSetForegroundArgs);
-    }
-    
     @Override
     public void onCreate() {
         instance = this;
-        mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        try {
-            mStartForeground = getClass().getMethod("startForeground", mStartForegroundSignature);
-            mStopForeground = getClass().getMethod("stopForeground", mStopForegroundSignature);
-        } catch (NoSuchMethodException e) {
-            // Running on an older platform.
-            mStartForeground = mStopForeground = null;
-            return;
-        }
-        try {
-            mSetForeground = getClass().getMethod("setForeground", mSetForegroundSignature);
-        } catch (NoSuchMethodException e) {
-            throw new IllegalStateException("OS doesn't have Service.startForeground OR Service.setForeground!");
-        }
     }
     
     @Override
@@ -152,30 +65,16 @@ public class ForegroundService extends Service implements ICacheEndListener {
      */
     public void finishService() {
         if (instance != null) {
-            // Remove the notification
-            stopForegroundCompat(R.string.Cache_service_started);
-            
-            // Call all activities
+            stopForeground(true);
             Controller.getInstance().notifyActivities();
-            
-            // Reset Instance
             instance = null;
         }
-    }
-    
-    // This is the old onStart method that will be called on the pre-2.0
-    // platform. On 2.0 or later we override onStartCommand() so this
-    // method will not be called.
-    @Override
-    public void onStart(Intent intent, int startId) {
-        handleCommand(intent);
     }
     
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         handleCommand(intent);
-        // We want this service to continue running until it is explicitly
-        // stopped, so return sticky.
+        // We want this service to continue running until it is explicitly stopped, so return sticky.
         return START_STICKY;
     }
     
@@ -202,7 +101,7 @@ public class ForegroundService extends Service implements ICacheEndListener {
         // Display notification
         Notification notification = Utils.buildNotification(getApplicationContext(), icon, ticker, title, text, true,
                 new Intent());
-        startForegroundCompat(R.string.Cache_service_started, notification);
+        startForeground(R.string.Cache_service_started, notification);
     }
     
     @Override
