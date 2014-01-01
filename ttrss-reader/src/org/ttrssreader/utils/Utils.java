c2dfbde2938b309538caf398c0e59ee3b629cc30
nils.braden <nils.braden@localhost>
2014-01-01 22:55:50 +0100

    Reduced article limit to speed up db operations, 5k should be enough

--- ttrss-reader/src/org/ttrssreader/utils/Utils.java
+++ ttrss-reader/src/org/ttrssreader/utils/Utils.java
@@ -62,7 +62,7 @@ public class Utils {
     /**
      * The maximum number of articles to store.
      */
-    public static final int ARTICLE_LIMIT = 8000;
+    public static final int ARTICLE_LIMIT = 5000;
     
     /**
      * Min supported versions of the Tiny Tiny RSS Server
