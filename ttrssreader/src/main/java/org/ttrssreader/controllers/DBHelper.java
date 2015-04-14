nilsbraden/ttrss-reader-fork

commit bc66830b90c5bb195b6d87aed946aaa9ed3906c1

Performance: Attempt to reduce access on DB while downloading images by collecting values and bulk inserting

ttrssreader/src/main/java/org/ttrssreader/imageCache/ImageCacher.java
@@ -66,6 +66,9 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 	private int taskCount = 0;
 
 	private Map<Integer, DownloadImageTask> map = new HashMap<>();
+	// Cache values and insert them later:
+	Map<Integer, String[]> articleFiles = new HashMap<>();
+	Map<String, Long> remoteFiles = new HashMap<>();
 
 	ImageCacher(ICacheEndListener parent, final Context context, boolean onlyArticles) {
 		this.parent = parent;
@@ -271,6 +274,19 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 			}
 		}
 
+		// Insert cached values:
+		for (Integer articleId : articleFiles.keySet()) {
+			DBHelper.getInstance().insertArticleFiles(articleId, articleFiles.get(articleId));
+		}
+		for (String url : remoteFiles.keySet()) {
+			Long size = remoteFiles.get(url);
+			if (size <= 0) {
+				DBHelper.getInstance().markRemoteFileCached(url, false, -size);
+			} else {
+				DBHelper.getInstance().markRemoteFileCached(url, true, size);
+			}
+		}
+
 		Log.i(TAG, "Downloading images took " + (System.currentTimeMillis() - time) + "ms");
 	}
 
@@ -281,6 +297,9 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 		private ImageCache imageCache;
 		private int articleId;
 		private String[] fileUrls;
+		// Thread-Local cache:
+		Map<Integer, String[]> articleFilesLocal = new HashMap<>();
+		Map<String, Long> remoteFilesLocal = new HashMap<>();
 
 		private DownloadImageTask(ImageCache cache, int articleId, String... params) {
 			this.imageCache = cache;
@@ -293,21 +312,19 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 			long size = 0;
 			try {
 				Log.d(TAG, "maxFileSize = " + maxFileSize + " and minFileSize = " + minFileSize);
-				DBHelper.getInstance().insertArticleFiles(articleId, fileUrls);
+				articleFilesLocal.put(articleId, fileUrls);
 				for (String url : fileUrls) {
 					size = FileUtils.downloadToFile(url, imageCache.getCacheFile(url), maxFileSize, minFileSize);
-
-					if (size <= 0) {
-						DBHelper.getInstance().markRemoteFileCached(url, false, -size);
-					} else {
-						DBHelper.getInstance().markRemoteFileCached(url, true, size);
-					}
+					remoteFilesLocal.put(url, size);
 				}
 			} catch (Throwable t) {
 				t.printStackTrace();
 			} finally {
 				synchronized (map) {
 					if (downloaded > 0) downloaded += size;
+
+					remoteFiles.putAll(remoteFilesLocal);
+					remoteFiles.putAll(remoteFilesLocal);
 
 					map.remove(articleId);
 					publishProgress(++progressImageDownload);

localeapi/localeapi.iml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<module external.linked.project.path="$MODULE_DIR$" external.root.project.path="$MODULE_DIR$/.." external.system.id="GRADLE" external.system.module.group="ttrss-reader-fork" external.system.module.version="unspecified" type="JAVA_MODULE" version="4">
+<module external.linked.project.id=":localeapi" external.linked.project.path="$MODULE_DIR$" external.root.project.path="$MODULE_DIR$/.." external.system.id="GRADLE" external.system.module.group="ttrss-reader-fork" external.system.module.version="unspecified" type="JAVA_MODULE" version="4">
   <component name="FacetManager">
     <facet type="android-gradle" name="Android-Gradle">
       <configuration>
@@ -12,8 +12,9 @@
         <option name="SELECTED_TEST_ARTIFACT" value="_android_test_" />
         <option name="ASSEMBLE_TASK_NAME" value="assembleDebug" />
         <option name="COMPILE_JAVA_TASK_NAME" value="compileDebugSources" />
-        <option name="ASSEMBLE_TEST_TASK_NAME" value="assembleDebugTest" />
         <option name="SOURCE_GEN_TASK_NAME" value="generateDebugSources" />
+        <option name="ASSEMBLE_TEST_TASK_NAME" value="assembleDebugTest" />
+        <option name="COMPILE_JAVA_TEST_TASK_NAME" value="compileDebugTestSources" />
         <option name="TEST_SOURCE_GEN_TASK_NAME" value="generateDebugTestSources" />
         <option name="ALLOW_USER_CONFIGURATION" value="false" />
         <option name="MANIFEST_FILE_RELATIVE_PATH" value="/src/main/AndroidManifest.xml" />
@@ -88,4 +89,3 @@
     <orderEntry type="sourceFolder" forTests="false" />
   </component>
 </module>
-

ttrssreader/ttrssreader.iml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<module external.linked.project.path="$MODULE_DIR$" external.root.project.path="$MODULE_DIR$/.." external.system.id="GRADLE" external.system.module.group="ttrss-reader-fork" external.system.module.version="unspecified" type="JAVA_MODULE" version="4">
+<module external.linked.project.id=":ttrssreader" external.linked.project.path="$MODULE_DIR$" external.root.project.path="$MODULE_DIR$/.." external.system.id="GRADLE" external.system.module.group="ttrss-reader-fork" external.system.module.version="unspecified" type="JAVA_MODULE" version="4">
   <component name="FacetManager">
     <facet type="android-gradle" name="Android-Gradle">
       <configuration>
@@ -12,8 +12,9 @@
         <option name="SELECTED_TEST_ARTIFACT" value="_android_test_" />
         <option name="ASSEMBLE_TASK_NAME" value="assembleDebug" />
         <option name="COMPILE_JAVA_TASK_NAME" value="compileDebugSources" />
-        <option name="ASSEMBLE_TEST_TASK_NAME" value="assembleDebugTest" />
         <option name="SOURCE_GEN_TASK_NAME" value="generateDebugSources" />
+        <option name="ASSEMBLE_TEST_TASK_NAME" value="assembleDebugTest" />
+        <option name="COMPILE_JAVA_TEST_TASK_NAME" value="compileDebugTestSources" />
         <option name="TEST_SOURCE_GEN_TASK_NAME" value="generateDebugTestSources" />
         <option name="ALLOW_USER_CONFIGURATION" value="false" />
         <option name="MANIFEST_FILE_RELATIVE_PATH" value="/src/main/AndroidManifest.xml" />
@@ -96,4 +97,3 @@
     <orderEntry type="module" module-name="localeapi" exported="" />
   </component>
 </module>
-

commit 9d2171065b38539f8b52267855b978e1d776a8aa

Fixed problem with ImageCache introduced with Revision bc66830b90c5bb195b6d87aed946aaa9ed3906c1

ttrssreader/src/main/java/org/ttrssreader/controllers/DBHelper.java
@@ -1842,22 +1842,29 @@ public class DBHelper {
 		}
 	}
 
+	private void insertArticleFiles(int articleId, String[] fileUrls) {
+		if (!isDBAvailable()) return;
+
+		for (String url : fileUrls) {
+			long remotefileId = insertRemoteFile(url);
+			if (remotefileId != 0) insertRemoteFile2Article(remotefileId, articleId);
+		}
+	}
+
 	/**
 	 * insert given remote files into DB and link them with given article
 	 *
-	 * @param articleId "parent" article
-	 * @param fileUrls  array of remote file URLs
+	 * @param map A map of arrays of remote file URLs mapped to ids of "parent" articles
 	 */
-	public void insertArticleFiles(int articleId, String[] fileUrls) {
+	public void insertArticleFiles(Map<Integer, String[]> map) {
 		if (!isDBAvailable()) return;
 
 		SQLiteDatabase db = getOpenHelper().getWritableDatabase();
 		writeLock(true);
 		db.beginTransaction();
 		try {
-			for (String url : fileUrls) {
-				long remotefileId = insertRemoteFile(url);
-				if (remotefileId != 0) insertRemoteFile2Article(remotefileId, articleId);
+			for (Integer articleId : map.keySet()) {
+				insertArticleFiles(articleId, map.get(articleId));
 			}
 			db.setTransactionSuccessful();
 		} finally {
@@ -2042,23 +2049,29 @@ public class DBHelper {
 	/**
 	 * mark given remote file as cached/uncached and optionally specify it's file size
 	 *
-	 * @param url    remote file URL
-	 * @param cached the cached flag
-	 * @param size   file size may be {@code null}, if so, then it will not be updated in DB
+	 * @param remoteFiles A map of file sizes mapped to their remote file URL
 	 */
-	public void markRemoteFileCached(String url, boolean cached, Long size) {
+	public void markRemoteFilesCached(Map<String, Long> remoteFiles) {
 		if (!isDBAvailable()) return;
 
 		SQLiteDatabase db = getOpenHelper().getWritableDatabase();
 		writeLock(true);
 		db.beginTransaction();
 		try {
-			ContentValues cv = new ContentValues(2);
-			cv.put("cached", cached);
-			if (size != null) {
-				cv.put("length", size);
+
+			for (String url : remoteFiles.keySet()) {
+				ContentValues cv = new ContentValues(2);
+				Long size = remoteFiles.get(url);
+				if (size <= 0) {
+					cv.put("cached", false);
+					cv.put("length", -size);
+				} else {
+					cv.put("cached", true);
+					cv.put("length", size);
+				}
+				db.update(TABLE_REMOTEFILES, cv, "url=?", new String[] {url});
 			}
-			db.update(TABLE_REMOTEFILES, cv, "url=?", new String[] {url});
+
 			db.setTransactionSuccessful();
 		} finally {
 			db.endTransaction();

ttrssreader/src/main/java/org/ttrssreader/imageCache/ImageCacher.java
@@ -275,17 +275,8 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 		}
 
 		// Insert cached values:
-		for (Integer articleId : articleFiles.keySet()) {
-			DBHelper.getInstance().insertArticleFiles(articleId, articleFiles.get(articleId));
-		}
-		for (String url : remoteFiles.keySet()) {
-			Long size = remoteFiles.get(url);
-			if (size <= 0) {
-				DBHelper.getInstance().markRemoteFileCached(url, false, -size);
-			} else {
-				DBHelper.getInstance().markRemoteFileCached(url, true, size);
-			}
-		}
+		DBHelper.getInstance().insertArticleFiles(articleFiles);
+		DBHelper.getInstance().markRemoteFilesCached(remoteFiles);
 
 		Log.i(TAG, "Downloading images took " + (System.currentTimeMillis() - time) + "ms");
 	}
@@ -323,7 +314,7 @@ class ImageCacher extends AsyncTask<Void, Integer, Void> {
 				synchronized (map) {
 					if (downloaded > 0) downloaded += size;
 
-					remoteFiles.putAll(remoteFilesLocal);
+					articleFiles.putAll(articleFilesLocal);
 					remoteFiles.putAll(remoteFilesLocal);
 
 					map.remove(articleId);