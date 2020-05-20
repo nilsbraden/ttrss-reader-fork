cc29a5d3e055c35ee95aec890afd4122463ef7dd
nilsbraden <ttrss@nilsbraden.de>
2020-05-20 22:31:49 +0200

    Fix username/password fields, no line breaks anymore and masked the password again

--- ttrssreader/src/main/java/org/ttrssreader/preferences/fragments/ConnectionPreferencesFragment.java
+++ ttrssreader/src/main/java/org/ttrssreader/preferences/fragments/ConnectionPreferencesFragment.java
@@ -1,10 +1,14 @@
 package org.ttrssreader.preferences.fragments;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
 
 import org.ttrssreader.R;
 
 import androidx.annotation.Keep;
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 @Keep
@@ -22,5 +26,25 @@ public class ConnectionPreferencesFragment extends PreferenceFragmentCompat {
 	public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
 		addPreferencesFromResource(R.xml.prefs_connection);
 		WifiPreferencesFragment.tryInitializeWifiPrefs(getContext(), getArguments(), getPreferenceScreen());
+
+		EditTextPreference prefUsername = findPreference("ConnectionUsernamePreference");
+		if (prefUsername != null) {
+			prefUsername.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+				@Override
+				public void onBindEditText(@NonNull EditText editText) {
+					editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+				}
+			});
+		}
+
+		EditTextPreference prefPassword = findPreference("ConnectionPasswordPreference");
+		if (prefPassword != null) {
+			prefPassword.setOnBindEditTextListener(new EditTextPreference.OnBindEditTextListener() {
+				@Override
+				public void onBindEditText(@NonNull EditText editText) {
+					editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+				}
+			});
+		}
 	}
 }
